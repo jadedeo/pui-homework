0fb2beef3eeafac0c8bc8992ca26cc16c45cc747
aogle17 <aogle@andrew.cmu.edu>
2024-10-08 21:35:36 -0400

    updating

--- solution-hw5/js/cart.js
+++ solution-hw5/js/cart.js
@@ -25,7 +25,6 @@ class Roll {
          
        if(this.glazing == glazingOptions[i].glaze){
            this.glazePrice = glazingOptions[i].glazingPrice;
-           console.log(this.glazePrice);
          }
        }
      }
@@ -35,7 +34,6 @@ class Roll {
         
         if(this.size == packOptions[i].packSize){
             this.packPrice = packOptions[i].packPrice;
-            console.log(this.packPrice);
         }
       }
     }
